6b4ff975cb9ff0579014986397f0fc7702f4b032
samuilth01 <102425504+samuilth01@users.noreply.github.com>
2023-06-05 01:14:10 +0300

    Add cubeController

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,6 +5,9 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/about', (req, res) => {
+    res.render('about');
+})
 
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebasConfig');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
+
 const app = express();
 
 const PORT = 5000;
@@ -11,5 +13,6 @@ expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => console.log(`Server i running on port ${PORT}...`));
